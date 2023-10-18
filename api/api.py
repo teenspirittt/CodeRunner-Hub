18e64fc3f4137cf5071b038f0698492e8f1e2183
teenspirittt <vshabunin25@gmail.com>
2023-10-18 19:13:08 +0700

    change to mvc

--- api/api.py
+++ api/api.py
@@ -1,31 +1,11 @@
-from flask import Flask, request, jsonify
-from json_parser import parse_json
-from response_builder import build_response
-from mongodb.handler import establish_connection, save_to_mongodb, close_connection
+from flask import Flask
+from api.routes.assignment_routes import assignment_routes
 
 app = Flask(__name__)
 
-@app.route('/execute', methods=['GET'])
-def execute_code():
-    try:
-        json_data = request.get_json()
-        if not json_data:
-            return jsonify({"error": "Invalid JSON data"}), 400
-
-        data = parse_json(json_data)
-        client = establish_connection()
-
-        saved = save_to_mongodb(data['student_id'], data['problem_id'], data['programming_language'], data['assignment_code'])
-        
-
-        if not saved:
-            close_connection(client)
-            return jsonify({"error": "Failed to save data to MongoDB"}), 500
-        
-        return jsonify("meow"), 200
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+# Регистрируем маршруты из assignment_routes
+app.register_blueprint(assignment_routes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
---
+++ api/routes/assignment_routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+from api.controllers.assignment_controller import execute_code, get_assignment_code
+
+assignment_routes = Blueprint("assignment_routes", __name__)
+
+@assignment_routes.route('/execute', methods=['POST'])
+def execute_code():
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({"error": "Invalid JSON data"}), 400
+
+    result = execute_code(json_data)
+    return jsonify(result), 200
+
+@assignment_routes.route('/get_code', methods=['GET'])
+def get_assignment():
+    student_id = request.args.get('student_id')
+    problem_id = request.args.get('problem_id')
+
+    code = get_assignment_code(student_id, problem_id)
+    if code is None:
+        return jsonify({"error": "Assignment not found"}), 404
+    return jsonify({"code": code}), 200
