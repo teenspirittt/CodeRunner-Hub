cedb683ca0b9e25903bee11da14d3f75966611bf
teenspirittt <vshabunin25@gmail.com>
2023-10-18 17:50:39 +0700

    add get_problem by id

--- mongodb/handler.py
+++ mongodb/handler.py
@@ -8,19 +8,30 @@ def save_to_mongodb(client, student_id, problem_id, programming_language, code):
     try:
         db = client["codes"]
         collection = db["problems"]
-        assignment_data = {
+        problem_data = {
             "student_id": student_id,
             "problem_id": problem_id,
             "programming_language": programming_language,
             "code": code
         }
-        
-        collection.insert_one(assignment_data)
-
+        collection.insert_one(problem_data)
         return True
+    
     except Exception as e:
         print(f"Error while saving to MongoDB: {str(e)}")
         return False
+
+
+def get_problem_by_student(client, student_id, problem_id):
+    try:
+        db = client["codes"]
+        collection = db["problems"]
+        problem = collection.find_one({"student_id": student_id, "problem_id": problem_id})
+        return problem
+    
+    except Exception as e:
+        print(f"Error while getting problem from MongoDB: {str(e)}")
+        return None
 
 def close_connection(client):
 
