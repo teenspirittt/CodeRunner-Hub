a71fe3b6baf45725d7881265dadaa0c5802f9785
teenspirittt <vshabunin25@gmail.com>
2023-10-18 19:13:30 +0700

    change to mvc

--- api/api.py
+++ api/api.py
@@ -3,9 +3,8 @@ from api.routes.assignment_routes import assignment_routes
 
 app = Flask(__name__)
 
-# Регистрируем маршруты из assignment_routes
+
 app.register_blueprint(assignment_routes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
