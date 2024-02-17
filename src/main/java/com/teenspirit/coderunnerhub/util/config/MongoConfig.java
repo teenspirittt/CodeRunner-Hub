9e5939eb4d5231505ab6d27662d1ec805538b2dc
teenspirittt <vshabunin25@gmail.com>
2024-02-17 17:29:07 +0700

    mongo config

--- src/main/java/com/teenspirit/coderunnerhub/util/config/MongoConfig.java
+++ src/main/java/com/teenspirit/coderunnerhub/util/config/MongoConfig.java
@@ -3,25 +3,49 @@ package com.teenspirit.coderunnerhub.util.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@EnableMongoRepositories(
+        basePackages = "com.teenspirit.coderunnerhub.repository.mongodb",
+        mongoTemplateRef = "mongoTemplate"
+)
 public class MongoConfig {
 
     @Value("${spring.data.mongodb.uri}")
     private String mongoURL;
 
+    @Bean(name = "mongoProperties")
+    @ConfigurationProperties(prefix = "spring.data.mongodb")
+    public MongoProperties mongoProperties() {
+        return new MongoProperties();
+    }
+
+
+    @Primary
     @Bean(name = "mongoDatabaseFactory")
     public MongoDatabaseFactory mongoDatabaseFactory() {
         return new SimpleMongoClientDatabaseFactory(mongoURL);
     }
 
+    @Primary
     @Bean(name = "mongoTemplate")
     public MongoTemplate mongoTemplate(@Qualifier("mongoDatabaseFactory") MongoDatabaseFactory mongoDatabaseFactory) {
         return new MongoTemplate(mongoDatabaseFactory);
+    }
+
+    @Primary
+    @Bean (name = "transactionManager")
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTransactionManager(mongoDatabaseFactory);
     }
 }
